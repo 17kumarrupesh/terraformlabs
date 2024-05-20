554bffdaa05dd38c3d91dbb2967b487d12dfb0c9
17kumarrupesh <55622346+17kumarrupesh@users.noreply.github.com>
2024-05-20 07:09:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,19 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
+# Data block to reference the existing resource group
+data "azurerm_resource_group" "existing" {
+  name = var.resource_group_name
 }
+#resource "azurerm_resource_group" "example" {
+#  name     = var.resource_group_name
+#  location = var.location
+#}
 
 resource "azurerm_storage_account" "example" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = data.azurerm_resource_group.existing.name
+  location                 = data.azurerm_resource_group.existing.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
