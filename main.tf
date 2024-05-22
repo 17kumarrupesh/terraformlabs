34ab78387fb27f3c792990ee7a28820e7b5a70ce
17kumarrupesh <55622346+17kumarrupesh@users.noreply.github.com>
2024-05-22 16:47:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,30 @@ provider "azurerm" {
   features {}
 }
 
-# Data block to reference the existing resource group
-data "azurerm_resource_group" "existing" {
-  name = var.resource_group_name
+# Define the existing resource group
+resource "azurerm_resource_group" "existingrg" {
+  name     = var.resource_group_name
+  location = var.location
 }
-#resource "azurerm_resource_group" "example" {
-#  name     = var.resource_group_name
-#  location = var.location
-#}
 
+# Define the storage account to be created in the existing resource group
 resource "azurerm_storage_account" "example" {
   name                     = var.storage_account_name
-  resource_group_name      = data.azurerm_resource_group.existing.name
-  location                 = data.azurerm_resource_group.existing.location
+  resource_group_name      = azurerm_resource_group.existing.name
+  location                 = azurerm_resource_group.existing.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
   tags = var.tags
+}
+
+# Outputs
+output "storage_account_id" {
+  description = "The ID of the Storage Account"
+  value       = azurerm_storage_account.example.id
+}
+
+output "storage_account_primary_endpoint" {
+  description = "The primary endpoint for the Storage Account"
+  value       = azurerm_storage_account.example.primary_blob_endpoint
 }
